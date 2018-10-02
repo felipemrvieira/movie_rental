507ea936d8d7d974ad986a904bc1743af8c3c232
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2018-10-02 11:34:18 -0300

    Creates the scaffolds
    This commit creates the scaffolds of Rental and Movie, adds a form.select on the partial _form and adjust params on rental_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :rentals
+  resources :movies
   devise_for :users
+  root to: "rentals#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
